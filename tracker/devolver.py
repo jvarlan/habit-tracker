81346a1d55e7bbb64a8be0e3ec719233ecc1f882
jvarlan <jvarlan1998@gmail.com>
2026-03-05 12:13:32 +0000

    - Se empieza el desarrollo del menú modificar: - Este se muestra completo, pero solo funciona la primera opción: Modificar un hábito

--- tracker/devolver.py
+++ tracker/devolver.py
@@ -13,6 +13,7 @@ def dev_habito_id(habito):
         for fila in lector:
             if fila[1].lower() == habito.lower():
                 return fila[0]
+            
 def dev_temporizador_id(temporizador):
     ruta = BASE_DIR / "datos" / "temporizadores.csv"
 
@@ -26,6 +27,7 @@ def dev_temporizador_id(temporizador):
         for fila in lector:
             if fila[2] == temporizador:
                 return fila[0]
+            
 def dev_lista_habitos_cat(id_categoria):
     ruta = BASE_DIR / "datos" / "habitos.csv"
 
@@ -40,6 +42,7 @@ def dev_lista_habitos_cat(id_categoria):
             if fila[2] == id_categoria:
                 lista.append(fila) 
         return lista  
+    
 def dev_lista_temporizadores_cat(lista_habitos,id_categoria):
     ruta = BASE_DIR / "datos" / "temporizadores.csv"
     
