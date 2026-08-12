9677efbe0b978379b6e10e77e13702ae798cd81d
jvarlan <jvarlan1998@gmail.com>
2026-08-12 13:24:21 +0000

    La primera vez, se crean los CSV también

--- main.py
+++ main.py
@@ -3,6 +3,20 @@ import os
 # Crear la carpeta de datos si no existe
 os.makedirs("datos", exist_ok=True)
 
+# Crear los CSV si no existen
+archivos_datos = [
+    "categorias.csv",
+    "habitos.csv",
+    "objetivos.csv",
+    "temporizadores.csv"
+]
+
+for archivo in archivos_datos:
+    ruta = os.path.join("datos", archivo)
+    if not os.path.exists(ruta):
+        open(ruta, "w", encoding="utf-8").close()
+
+
 from tracker import mostrar_menu
 
 
