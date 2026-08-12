93ca9fb7700466f27edf01afa776217fc43a09be
jvarlan <jvarlan1998@gmail.com>
2026-08-12 13:19:48 +0000

    Crear carpeta datos la primera vez

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import os
+
+# Crear la carpeta de datos si no existe
+os.makedirs("datos", exist_ok=True)
+
 from tracker import mostrar_menu
 
 
 def main():
     # llama al menu principal
     mostrar_menu()
-    
+
+
 if __name__ == "__main__":
     main()
